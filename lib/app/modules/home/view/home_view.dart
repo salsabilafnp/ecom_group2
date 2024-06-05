03c1fe8aa1766ff1244f9c4c89ba359924bb0bed
Rian Alfazri Khoirulloh <rian.alfazri96@gmail.com>
2024-06-05 20:07:56 +0700

    initiating

---
+++ lib/app/modules/home/view/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [TextField()],
+        ),
+      ),
+    );
+  }
+}
