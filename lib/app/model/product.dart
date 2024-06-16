d8f0511d32b60ff3b91039965d64abc7db06f9fa
Oneday37 <everysummertime00@gmail.com>
2024-06-16 17:18:48 +0700

    Favorite UI clear

--- lib/app/model/product.dart
+++ lib/app/model/product.dart
@@ -1,28 +1,39 @@
+class Rating {
+  final dynamic rate;
+  final int count;
+
+  Rating({required this.rate, required this.count});
+  factory Rating.fromJson(Map<String, dynamic> json) {
+    return Rating(rate: json['rate'], count: json['count']);
+  }
+}
+
 class Product {
   final int id;
   final String title;
-  final double price;
+  final dynamic price;
   final String description;
   final String category;
   final String image;
+  Rating rating;
 
-  Product({
-    required this.id,
-    required this.title,
-    required this.price,
-    required this.description,
-    required this.category,
-    required this.image,
-  });
+  Product(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.category,
+      required this.image,
+      required this.rating});
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      id: json['id'],
-      title: json['title'],
-      price: json['price'].toDouble(),
-      description: json['description'],
-      category: json['category'],
-      image: json['image'],
-    );
+        id: json['id'],
+        title: json['title'],
+        price: json['price'],
+        description: json['description'],
+        category: json['category'],
+        image: json['image'],
+        rating: Rating.fromJson(json['rating']));
   }
 }
