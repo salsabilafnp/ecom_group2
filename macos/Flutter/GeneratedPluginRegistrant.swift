64d495d42fd7c6b6f619ac739205c6b0c60cf418
Rian Alfazri Khoirulloh <rian.alfazri96@gmail.com>
2024-06-16 21:12:54 +0700

    update dikit

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,9 +10,3 @@ import shared_preferences_foundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
-
-import file_selector_macos
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-}
